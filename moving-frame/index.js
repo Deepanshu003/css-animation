dec37c74c7e7579532d648044b67b774346788b9
Deepanshu003 <91359646+Deepanshu003@users.noreply.github.com>
2023-04-01 12:08:04 -0700

    Add files via upload

---
+++ moving-frame/index.js
@@ -0,0 +1,8 @@
+// const car = document.getElementById("missile");
+// const stations = document.querySelectorAll(".station");
+// stations.forEach((station, index) => {
+//     setTimeout(() => {
+//       car.style.transform = `translateX(${station.offsetLeft}px)`;
+      
+//     }, index * 3000);
+// });
