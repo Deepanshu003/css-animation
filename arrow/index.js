2a229a6dcf412dd5cf262ad399fb467411fdf6ac
Deepanshu003 <91359646+Deepanshu003@users.noreply.github.com>
2023-04-02 11:01:38 -0700

    Add files via upload

---
+++ arrow/index.js
@@ -0,0 +1,9 @@
+// const arrow = document.getElementById("arrow");
+// const target = document.getElementById("target");
+
+// target.forEach((station, index) => {
+//     setTimeout(() => {
+//       arrow.style.transform = `translateX(${station.offsetLeft}px)`;
+      
+//     }, index * 3000);
+// });
